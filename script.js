9da4b6ccaad87342dca0b023b7e05290d37323cb
Baptiste-Madelaine <madelainebaptiste61@gmail.com>
2023-09-11 18:44:52 +0200

    patching reset function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const min = document.getElementById("min");
 const sec = document.getElementById("sec");
 const state = document.getElementById("state");
 const button = document.getElementById("but");
+const pause = document.getElementById("pause");
 let tempTot = 0;
 let tempPause = 0;
 let onWork = false;
@@ -13,26 +14,45 @@ function State(){
     state.textContent = onWork ? "Travail !":"Pause !";
     console.log(state)
 }
-
+function Reset(){
+    onWork = true;
+    State();
+}
 function Click(){
     tempTot = temp.value*60;
+    tempPause = pause.value*60;
     console.log(tempTot);
     if(!id_interval){
         id_interval = setInterval(Decr, 1000);
         onWork = !onWork;
         State();
         button.value = button.value =="Reset" ? "Start":"Reset";
+    }else{
+        Reset();
     }
 }
+
 function Decr(){
     if(onWork){
         tempTot--;
-        
+        sec.textContent = tempTot%60 < 10? "0" + tempTot%60:tempTot%60;
+        min.textContent= Math.trunc(tempTot/60)<10 ? "0"+Math.trunc(tempTot/60)+" :":Math.trunc(tempTot/60) + " :";
     }else{
         tempPause--;
         sec.textContent = tempPause%60 < 10? "0" + tempPause%60:tempPause%60;
         min.textContent= Math.trunc(tempPause/60)<10 ? "0"+Math.trunc(tempPause/60)+" :":Math.trunc(tempPause/60) + " :";
     }
+    if(tempTot==0 || tempPause==0 ){
+        tempPause = pause.value*60;
+        tempTot = temp.value*60;
+        onWork = !onWork;
+        State();
+    }
     //console.log(tempTot);
 }
+
+temp.addEventListener("change",(evt)=>{
+    min.textContent = temp.value+":";
+})
 State();
+min.textContent = temp.value+":";
